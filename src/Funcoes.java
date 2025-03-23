3b4e22bb6367a2b3b7855a695288880a865b9c7f
zulang21 <arthurricken1@gmail.com>
2025-03-23 13:35:07 -0300

    correçao da funçao

--- src/Funcoes.java
+++ src/Funcoes.java
@@ -2,10 +2,12 @@ public class Funcoes {
     public static int[] removeDuplicatas(int[] vetor) {
         int vetorAuxiliar[] = new int[vetor.length];
         int contador = 0;
+        int repetidas = 0;
         for (int x = 0; x < vetor.length; x++) {
             if (x > 0) {
                 if (vetor[x] == vetor[x - 1]) {
                     vetorAuxiliar[contador] = 0;
+                    repetidas++;
 
                 } else {
                     vetorAuxiliar[contador] = vetor[x];
@@ -16,6 +18,7 @@ public class Funcoes {
                 contador++;
             }
         }
+        System.out.println("Foram removidos " + repetidas + " elementos repetidos");	
         return vetorAuxiliar;
     }
 }
