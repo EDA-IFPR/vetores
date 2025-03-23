1f5668333895d608469c933d4c7817617442a70f
zulang21 <arthurricken1@gmail.com>
2025-03-23 15:12:03 -0300

    update final

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int vetor[] = { 1, 1, 2, 2, 3 };
-        Funcoes funcoes = new Funcoes();
-        int vetor2[] = funcoes.removeDuplicatas(vetor);
+        int vetor2[] = Funcoes.removeDuplicatas(vetor);
         for (int x = 0; x < vetor2.length; x++) {
             System.out.println(vetor2[x]);
         }
