7bd7955a07f0a6c0007a809296ede2524352dc3d
Luqui05 <lucashtribeiro@hotmail.com>
2025-03-24 15:15:50 -0300

    Update Solution.java

--- src/is_anagram/Solution.java
+++ src/is_anagram/Solution.java
@@ -3,13 +3,11 @@ package is_anagram;
 public class Solution {
   public boolean isAnagram(String s, String t) {
     int[] contagem = new int[26];
-    char[] sw = s.toCharArray();
-    char[] tw = t.toCharArray();
-    for (int i = 0; i < sw.length; i++) {
-      contagem[(sw[i] - 'a')]++;
+    for (int i = 0; i < s.length(); i++) {
+      contagem[(s.charAt(i) - 'a')]++;
     }
-    for (int j = 0; j < sw.length; j++) {
-      contagem[(tw[j]) - 'a']--;
+    for (int j = 0; j < t.length(); j++) {
+      contagem[(t.charAt(j) - 'a')]--;
     }
 
     for (int c : contagem) {
